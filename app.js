9dca2ece21c0ae9a4a499c562cd8cba83d031523
Yunik Maharjan <yunik.maharjan@icloud.com>
2017-07-22 23:42:41 +0545

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+var index=window.location.href.indexOf("?");
+if(index!=-1){
+
+var q=window.location.href.slice(index+3);
+var url="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="+q+"&type=video&key=AIzaSyC9ta_5TtJcU3M6m0a5vtytSnpzESFbXRs";
+
+$.getJSON(url, function(data){
+var video=data.items[0].id.videoId;
+theurl="https://www.youtube.com/watch?v="+video;
+
+window.location.href = theurl;
+});
+
+}
+
+else{
+var theurl;
+$("form").on('submit',function(e){
+e.preventDefault();
+
+var q=encodeURIComponent($("#search_").val()).replace('/%20/g', "+");
+
+var url="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="+q+"&type=video&key=AIzaSyC9ta_5TtJcU3M6m0a5vtytSnpzESFbXRs";
+
+$.getJSON(url, function(data){
+var video=data.items[0].id.videoId;
+theurl="https://www.youtube.com/watch?v="+video;
+
+window.location.href = theurl;
+});
+
+});
+
+}
