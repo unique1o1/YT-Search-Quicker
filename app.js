1353cfbe139258f24a3040c9b7b0ce26d960e1d8
Yunik Maharjan <yunik.maharjan@icloud.com>
2017-07-23 00:23:15 +0545

    Minor changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 var index=window.location.href.indexOf("?");
-if(index!=-1){
+// if(index!=-1){
 
 var q=window.location.href.slice(index+3);
 var url="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="+q+"&type=video&key=AIzaSyC9ta_5TtJcU3M6m0a5vtytSnpzESFbXRs";
@@ -13,24 +13,24 @@ theurl="https://www.youtube.com/watch?v="+video;
 window.location.href = theurl;
 });
 
-}
+// }
 
-else{
-var theurl;
-$("form").on('submit',function(e){
-e.preventDefault();
+// else{
+// var theurl;
+// $("form").on('submit',function(e){
+// e.preventDefault();
 
-var q=encodeURIComponent($("#search_").val()).replace('/%20/g', "+");
+// var q=encodeURIComponent($("#search_").val()).replace('/%20/g', "+");
 
-var url="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="+q+"&type=video&key=AIzaSyC9ta_5TtJcU3M6m0a5vtytSnpzESFbXRs";
+// var url="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="+q+"&type=video&key=AIzaSyC9ta_5TtJcU3M6m0a5vtytSnpzESFbXRs";
 
-$.getJSON(url, function(data){
-var video=data.items[0].id.videoId;
-theurl="https://www.youtube.com/watch?v="+video;
+// $.getJSON(url, function(data){
+// var video=data.items[0].id.videoId;
+// theurl="https://www.youtube.com/watch?v="+video;
 
-window.location.href = theurl;
-});
+// window.location.href = theurl;
+// });
 
-});
+// });
 
-}
+// }
