9fda399efa5fff97c529177c09df4ca068afbe7b
Amit Chaudhary <meamitkc@gmail.com>
2017-07-23 07:40:57 +0545

    Use JSON object to hold query params

--- app.js
+++ app.js
@@ -3,13 +3,17 @@
 var index=window.location.href.indexOf("?");
 // if(index!=-1){
 
-var q=window.location.href.slice(index+3);
-var url="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="+q+"&type=video&key=AIzaSyC9ta_5TtJcU3M6m0a5vtytSnpzESFbXRs";
+var query=window.location.href.slice(index+3);
+
+var params = { part: "snippet", maxResults: 25, q: query, type: "video", key: "AIzaSyC9ta_5TtJcU3M6m0a5vtytSnpzESFbXRs"};
+
+var queryStrings = $.param(params);
+
+var url="https://www.googleapis.com/youtube/v3/search?" + queryStrings;
 
 $.getJSON(url, function(data){
 var video=data.items[0].id.videoId;
 theurl="https://www.youtube.com/watch?v="+video;
-
 window.location.href = theurl;
 });
 
