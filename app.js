919405a01d2ff2e298f1097c9f63448aa2cf27d7
Yunik Maharjan <yunik.maharjan@icloud.com>
2017-07-23 09:45:05 +0545

    imp

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var index=window.location.href.indexOf("?");
 // if(index!=-1){
 
 var q=window.location.href.slice(index+3);
-var url="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="+q+"&type=video&key=AIzaSyC9ta_5TtJcU3M6m0a5vtytSnpzESFbXRs";
+var url="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q="+q+"&type=video&key=AIzaSyC9ta_5TtJcU3M6m0a5vtytSnpzESFbXRs";
 
 $.getJSON(url, function(data){
 var video=data.items[0].id.videoId;
