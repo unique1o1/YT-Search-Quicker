f494c752e11a0d8f3756899a05d568ea1dbbc699
Amit Chaudhary <meamitkc@gmail.com>
2017-07-23 08:00:40 +0545

    Refactor query strings

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ var query=window.location.href.slice(index+3);
 
 var params = { part: "snippet", maxResults: 1, q: query, type: "video", key: "AIzaSyC9ta_5TtJcU3M6m0a5vtytSnpzESFbXRs", videoCategoryId: "10"};
 
-var queryStrings = $.param(params);
-
-var url="https://www.googleapis.com/youtube/v3/search?" + queryStrings;
+var url="https://www.googleapis.com/youtube/v3/search?" + $.param(params);
 
 $.getJSON(url, function(data){
 var video=data.items[0].id.videoId;
